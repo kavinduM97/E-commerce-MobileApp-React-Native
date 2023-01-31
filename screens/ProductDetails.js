5e468bf035e462678e3d49b72f6d58a922dfa86a
kavinduM97 <kavindumedhanga@gmail.com>
2023-01-31 22:24:22 +0530

    prodetails stage1

---
+++ screens/ProductDetails.js
@@ -0,0 +1,68 @@
+/* eslint-disable prettier/prettier */
+import React, {useEffect, useState} from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  ScrollView,
+  SafeAreaView,
+  Button,
+} from 'react-native';
+
+import {getProduct} from '../services/ProductService';
+
+export function ProductDetails({route}) {
+  const {productId} = route.params;
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    setProduct(getProduct(productId));
+  }, [productId]);
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.imageContainer}>
+          <Image style={styles.image} source={product.image} />
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.name}>{product.name}</Text>
+          <Text style={styles.price}>$ {product.price}</Text>
+          <Text style={styles.description}>{product.description}</Text>
+          <Button title="Add To Cart" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  infoContainer: {
+    padding: 16,
+  },
+  name: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  price: {
+    fontSize: 16,
+    fontWeight: '600',
+    marginBottom: 8,
+  },
+  description: {
+    fontSize: 16,
+    fontWeight: '400',
+    color: '#787878',
+    marginBottom: 16,
+  },
+});
